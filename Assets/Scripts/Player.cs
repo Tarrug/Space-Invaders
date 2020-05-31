1f239df7f5bbf291b8c3af0f6cc45dc80b5657f8
Tarrug <tarrugorton@gmail.com>
2020-05-31 19:10:41 +0700

    finished player control("player") script)

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 3.5f;   //player speed
+    
+
+    void Start()
+    {
+        transform.position = new Vector3(0,0,0);
+    }
+
+
+    void Update()
+    {
+        CalculateMovement();
+    }
+    void CalculateMovement() //player movement, area restriction
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical"); 
+            //meters per sec speed 
+        transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);  
+        transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);  
+            //(vertical axis movement restrictions)
+        transform.position = new Vector3(transform.position.x,Mathf.Clamp(transform.position.y,3.8f,0),0);
+            //(horizontal axis movement resrictions)
+        if (transform.position.x > 11.3f)
+        {
+            transform.position = new Vector3(-11.3f,transform.position.y,0);
+        }
+        else if (transform.position.x < -11.3f) 
+        {
+            transform.position = new Vector3(11.3f,transform.position.y,0);
+        }
+    }
+}
